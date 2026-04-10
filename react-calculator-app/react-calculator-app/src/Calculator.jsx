08227ca6028cf5a2e554371d12a4dae69ac534a4
sakaguchi <s.sakaguchi@unicrea.co.jp>
2026-04-10 16:49:30 +0900

    feat: useStateによる表示欄の状態管理(display)を実装

--- react-calculator-app/react-calculator-app/src/Calculator.jsx
+++ react-calculator-app/react-calculator-app/src/Calculator.jsx
@@ -1,24 +1,33 @@
+import { useState } from "react";
+
 export function Calculator() {
-    // ボタンの配置を表す配列
+    // memo:修正：[]（配列の分割代入）/ {}（オブジェクトの分割代入）
+    // displayという名前の状態変数を定義（初期値は空文字）
+    const [display, setDisplay] = useState("");
+
+    // ボタンの配置を表す配列（記述順に表示）
     const buttons = [
-        "7","8","9","/",
-        "4","5","6","*",
-        "1","2","3","-",
-        "0","C","=","+"
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', 'C', '=', '+'
     ];
 
-    const buttonList = buttons.map((btn) => (
-        <button key={btn}>{btn}</button>
-    ));
+    // 定義のみ
+    const handleClick = (btn) => {
+
+    };
 
     return (
         <div className="calculator-container">
             <h2>電卓アプリ</h2>
-            {/* 表示欄（ダミーで０を表示） */}
-            <div className="display">0</div>
-            {/* ボタンエリア */}
+            {/* displayをdiv要素に埋め込む。displayが空の場合は0とする。 */}
+            <div className="display">{display === "" ? "0" : display}</div>
+            {/* ボタンの定義 */}
             <div className="button-grid">
-                {buttonList}
+                {buttons.map((btn) => (
+                    <button key={btn} onClick={() => handleClick(btn)}>{btn}</button>
+                ))}
             </div>
         </div>
     );
